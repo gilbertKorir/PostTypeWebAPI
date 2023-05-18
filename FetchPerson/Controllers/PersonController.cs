c03273723f1cf270527640e4a531be5fcd03aa77
gilbertKorir <jerikorir@gmail.com>
2023-05-18 16:52:57 +0300

    ft;added the dependencies

--- FetchPerson/Controllers/PersonController.cs
+++ FetchPerson/Controllers/PersonController.cs
@@ -3,18 +3,19 @@ using System.Collections.Generic;
 using System.Data;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Net.Security;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web;
+using System.Web.DynamicData;
 using System.Web.Mvc;
 using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Shared;
 using FetchPerson.Models;
 using Newtonsoft.Json;
-using CrystalDecisions.CrystalReports;
-using CrystalDecisions.ReportSource;
-using CrystalDecisions.Shared;
+
 
 namespace FetchPerson.Controllers
 {
@@ -80,26 +81,86 @@ namespace FetchPerson.Controllers
 
         public ActionResult GetEmployeeReport()
         {
-            //get data
+            // Call the API to get the data and deserialize it to a list of PersonModel objects
             List<PersonModel> persons = new List<PersonModel>();
             HttpResponseMessage response = client.GetAsync("https://localhost:44368/api/person/fetch").Result;
             if (response.IsSuccessStatusCode)
             {
                 string res = response.Content.ReadAsStringAsync().Result;
-                _persons = JsonConvert.DeserializeObject<List<PersonModel>>(res);
+                persons = JsonConvert.DeserializeObject<List<PersonModel>>(res);
             }
 
-            //crystal report instance
+            // Create a new DataTable instance and add columns
+            DataTable dataTable = new DataTable("EmployeeTable");
+            dataTable.Columns.Add("Id", typeof(int));
+            dataTable.Columns.Add("Name", typeof(string));
+            dataTable.Columns.Add("Age", typeof(int));
+            dataTable.Columns.Add("Active", typeof(int));
+
+            // Add data to the DataTable
+            foreach (PersonModel person in persons)
+            {
+                DataRow row = dataTable.NewRow();
+                row["Id"] = person.Id;
+                row["Name"] = person.Name;
+                row["Age"] = person.Age;
+                row["Active"] = person.Active;
+                dataTable.Rows.Add(row);
+            }
+
+            // Create a new report document instance
             ReportDocument rd = new ReportDocument();
-            rd.Load(Server.MapPath("~/Reports/Employeereport.rpt"));
 
-            rd.SetDataSource(persons);
+            // Set the report source to the DataTable
+            rd.SetDataSource(dataTable);
 
-            //export report
+            // Export the report to a stream and return it as a PDF file
             Stream stream = rd.ExportToStream(ExportFormatType.PortableDocFormat);
             return File(stream, "application/pdf", "Employeereport.pdf");
 
         }
+
+        public ActionResult GenerateReport()
+        {
+                // Retrieve data from the Web API
+                HttpResponseMessage response = client.GetAsync("https://localhost:44368/api/person/fetch").Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    string res = response.Content.ReadAsStringAsync().Result;
+                    List<PersonModel> persons = JsonConvert.DeserializeObject<List<PersonModel>>(res);
+
+                    // Create an instance of the Crystal Report
+                    ReportDocument report = new ReportDocument();
+                    report.Load(Server.MapPath("D:/C-SHARP-projects/ApiPerson/FetchPerson/Reporting/EmployeeReport.rpt"));
+                   // report.Load(Server.MapPath("~/Reporting/EmployeeReport.rpt"));
+
+                    // Set the report data source
+                    report.SetDataSource(persons);
+
+                    // Set export options
+                    ExportOptions options = new ExportOptions();
+                    options.ExportFormatType = ExportFormatType.PortableDocFormat;
+                    options.ExportDestinationType = ExportDestinationType.DiskFile;
+                    options.DestinationOptions = new DiskFileDestinationOptions
+                    {
+                        DiskFileName = Server.MapPath("~/Reporting/EmployeeReport.pdf")
+                    };
+
+                //ReportDocumentOptions reportOptions = report.ReportOptions;
+                //reportOptions.ExportEncodingType = ExportEncodingType.NoEncoding;
+                // Export the report to PDF
+                report.Export(options);
+
+                    // Dispose the report object
+                    report.Close();
+                    report.Dispose();
+                }
+
+                return RedirectToAction("PersonAction"); // or any other appropriate action
+        }
+
+        
+
 
 
     }
